5f6ca9014230e02f62c5e88a32cc18406b519ed5
Aaron Clauson <aaron@sipsorcery.com>
2021-02-12 15:47:20 +0000

    Added aiortc webrtc echo test server.

---
+++ aiortc/server.py
@@ -0,0 +1,131 @@
+# A WebRTC Echo Test server that reflects back any audio or video frames it receives.
+# Originally based on https://github.com/aiortc/aiortc/blob/main/examples/server/server.py.
+
+import argparse
+import asyncio
+import json
+import logging
+import os
+import ssl
+import uuid
+
+from aiohttp import web
+from av import VideoFrame
+
+from aiortc import MediaStreamTrack, RTCPeerConnection, RTCSessionDescription
+from aiortc.contrib.media import MediaBlackhole
+
+ROOT = os.path.dirname(__file__)
+
+logger = logging.getLogger("pc")
+pcs = set()
+
+class EchoTrack(MediaStreamTrack):
+    """
+    A media stream track that reflects the same media frame it receives.
+    """
+
+    def __init__(self, track):
+        super().__init__()  # don't forget this!
+        self.track = track
+        self.kind = track.kind
+
+    async def recv(self):
+        frame = await self.track.recv()
+        return frame
+
+async def index(request):
+    content = open(os.path.join(ROOT, "index.html"), "r").read()
+    return web.Response(content_type="text/html", text=content)
+
+async def offer(request):
+    params = await request.json()
+
+    offer = RTCSessionDescription(sdp=params["sdp"], type=params["type"])
+
+    pc = RTCPeerConnection()
+    pc_id = "PeerConnection(%s)" % uuid.uuid4()
+    pcs.add(pc)
+
+    def log_info(msg, *args):
+        logger.info(pc_id + " " + msg, *args)
+
+    log_info("Created for %s", request.remote)
+
+    @pc.on("iceconnectionstatechange")
+    async def on_iceconnectionstatechange():
+        log_info("ICE connection state is %s", pc.iceConnectionState)
+        if pc.iceConnectionState == "failed":
+            await pc.close()
+            pcs.discard(pc)
+
+    @pc.on("track")
+    def on_track(track):
+        log_info("Track %s received", track.kind)
+
+        if track.kind == "audio":
+            local_audio = EchoTrack(track)
+            pc.addTrack(local_audio)
+        elif track.kind == "video":
+            local_video = EchoTrack(track)
+            pc.addTrack(local_video)
+
+        @track.on("ended")
+        async def on_ended():
+            log_info("Track %s ended", track.kind)
+
+    # handle offer
+    await pc.setRemoteDescription(offer)
+
+    # send answer
+    answer = await pc.createAnswer()
+    await pc.setLocalDescription(answer)
+
+    return web.Response(
+        content_type="application/json",
+        text=json.dumps(
+            {"sdp": pc.localDescription.sdp, "type": pc.localDescription.type}
+        ),
+    )
+
+
+async def on_shutdown(app):
+    # close peer connections
+    coros = [pc.close() for pc in pcs]
+    await asyncio.gather(*coros)
+    pcs.clear()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="WebRTC Echo Server demo"
+    )
+    parser.add_argument("--cert-file", help="SSL certificate file (for HTTPS)")
+    parser.add_argument("--key-file", help="SSL key file (for HTTPS)")
+    parser.add_argument(
+        "--host", default="0.0.0.0", help="Host for HTTP server (default: 0.0.0.0)"
+    )
+    parser.add_argument(
+        "--port", type=int, default=8080, help="Port for HTTP server (default: 8080)"
+    )
+    parser.add_argument("--verbose", "-v", action="count")
+    args = parser.parse_args()
+
+    if args.verbose:
+        logging.basicConfig(level=logging.DEBUG)
+    else:
+        logging.basicConfig(level=logging.INFO)
+
+    if args.cert_file:
+        ssl_context = ssl.SSLContext()
+        ssl_context.load_cert_chain(args.cert_file, args.key_file)
+    else:
+        ssl_context = None
+
+    app = web.Application()
+    app.on_shutdown.append(on_shutdown)
+    app.router.add_post("/offer", offer)
+    app.router.add_get("/", index)
+    web.run_app(
+        app, access_log=None, host=args.host, port=args.port, ssl_context=ssl_context
+    )
