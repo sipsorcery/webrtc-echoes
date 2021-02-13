f5c453b5247d9f69922293aaff53923bda9c6f48
Aaron Clauson <aaron@sipsorcery.com>
2021-02-13 16:10:29 +0000

    SIPSorcery WebRTC Echo test server.

---
+++ sipsorcery/Program.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text.Json;
+using System.Threading.Tasks;
+using EmbedIO;
+using SIPSorcery.Net;
+using SIPSorceryMedia.Abstractions;
+
+namespace webrtc_echo
+{
+    class Program
+    {
+        private const string DEFAULT_WEBSERVER_LISTEN_URL = "http://*:9696/";
+
+        private static List<IPAddress> _icePresets = new List<IPAddress>();
+
+        static void Main(string[] args)
+        {
+            var url = DEFAULT_WEBSERVER_LISTEN_URL;
+         
+            // Apply any command line options
+            if (args.Length > 0)
+            {
+                url = args[0];
+                for(int i=1; i<args.Length; i++)
+                {
+                    if(IPAddress.TryParse(args[i], out var addr))
+                    {
+                        _icePresets.Add(addr);
+                        Console.WriteLine($"ICE candidate preset address {addr} added.");
+                    }
+                }
+            }
+
+            // Start the web server.
+            using (var server = CreateWebServer(url))
+            {
+                server.RunAsync();
+                Console.WriteLine("Press any key to exit...");
+                Console.ReadKey(true);
+            }
+        }
+
+        private static WebServer CreateWebServer(string url)
+        {
+            var server = new WebServer(o => o
+                    .WithUrlPrefix(url)
+                    .WithMode(HttpListenerMode.EmbedIO))
+                .WithCors("*", "*", "*")
+                .WithAction("/offer", HttpVerbs.Post, Offer)
+                .WithStaticFolder("/", ".", false);
+            server.StateChanged += (s, e) => Console.WriteLine($"WebServer New State - {e.NewState}");
+
+            return server;
+        }
+
+        private async static Task Offer(IHttpContext context)
+        {
+            var offer = await context.GetRequestDataAsync<RTCSessionDescriptionInit>();
+
+            var jsonOptions = new JsonSerializerOptions();
+            jsonOptions.Converters.Add(new System.Text.Json.Serialization.JsonStringEnumConverter());
+
+            var echoServer = new WebRTCEchoServer(_icePresets);
+            var answer = await echoServer.GotOffer(offer);
+
+            if (answer != null)
+            {
+                context.Response.ContentType = "application/json";
+                using (var responseStm = context.OpenResponseStream(false, false))
+                {
+                    await JsonSerializer.SerializeAsync(responseStm, answer, jsonOptions);
+                }
+            }
+        }
+    }
+
+    public class WebRTCEchoServer
+    {
+        private const int VP8_PAYLOAD_ID = 96;
+
+        private List<IPAddress> _presetIceAddresses;
+
+        public WebRTCEchoServer(List<IPAddress> presetAddresses)
+        {
+            _presetIceAddresses = presetAddresses;
+        }
+
+        public async Task<RTCSessionDescriptionInit> GotOffer(RTCSessionDescriptionInit offer)
+        {
+            Console.WriteLine($"SDP offer received.");
+            //Console.WriteLine($"Offer SDP:\n{offer.sdp}");
+
+            var pc = new RTCPeerConnection();
+
+            if (_presetIceAddresses != null)
+            {
+                foreach (var addr in _presetIceAddresses)
+                {
+                    var rtpPort = pc.GetRtpChannel().RTPPort;
+                    var publicIPv4Candidate = new RTCIceCandidate(RTCIceProtocol.udp, addr, (ushort)rtpPort, RTCIceCandidateType.host);
+                    pc.addLocalIceCandidate(publicIPv4Candidate);
+                }
+            }
+
+            MediaStreamTrack audioTrack = new MediaStreamTrack(SDPWellKnownMediaFormatsEnum.PCMU);
+            pc.addTrack(audioTrack);
+            MediaStreamTrack videoTrack = new MediaStreamTrack(new VideoFormat(VideoCodecsEnum.VP8, VP8_PAYLOAD_ID));
+            pc.addTrack(videoTrack);
+
+            pc.OnRtpPacketReceived += (IPEndPoint rep, SDPMediaTypesEnum media, RTPPacket rtpPkt) =>
+            {
+                pc.SendRtpRaw(media, rtpPkt.Payload, rtpPkt.Header.Timestamp, rtpPkt.Header.MarkerBit, rtpPkt.Header.PayloadType);
+            };
+
+            pc.OnTimeout += (mediaType) => Console.WriteLine($"Timeout for {mediaType}.");
+            pc.oniceconnectionstatechange += (state) => Console.WriteLine($"ICE connection state changed to {state}.");
+            pc.onsignalingstatechange += () => Console.WriteLine($"Signaling state changed to {pc.signalingState}.");
+            pc.onconnectionstatechange += (state) =>
+            {
+                Console.WriteLine($"Peer connection state changed to {state}.");
+                if (state == RTCPeerConnectionState.failed)
+                {
+                    pc.Close("ice failure");
+                }
+            };
+
+            var setResult = pc.setRemoteDescription(offer);
+            if (setResult == SetDescriptionResultEnum.OK)
+            {
+                var offerSdp = pc.createOffer(null);
+                await pc.setLocalDescription(offerSdp);
+
+                var answer = pc.createAnswer(null);
+                await pc.setLocalDescription(answer);
+
+                //Console.WriteLine($"Answer SDP:\n{answer.sdp}");
+
+                return answer;
+            }
+            else
+            {
+                Console.WriteLine($"Failed to set remote description {setResult}.");
+                return null;
+            }
+        }
+    }
+}
