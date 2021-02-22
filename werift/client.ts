403efae78ef3d1600e7c4c19c9d270395c314f23
shinyoshiaki <shinyoshiaki2012@gmail.com>
2021-02-23 01:49:00 +0900

    impl client

--- werift/client.ts
+++ werift/client.ts
@@ -1,39 +1,34 @@
-import { RTCPeerConnection, RtpPacket } from "werift";
-import * as yargs from "yargs";
+import { RTCPeerConnection } from "werift";
 import axios from "axios";
 import { createSocket } from "dgram";
 
-// sender
-// ffmpeg -re -f lavfi -i testsrc=size=640x480:rate=30 -vcodec libvpx -keyint_min 30 -cpu-used 5 -deadline 1 -g 10 -error-resilient 1 -auto-alt-ref 1 -f rtp rtp://127.0.0.1:5000
-
-// receiver
-// gst-launch-1.0 -v udpsrc port=4002 caps = "application/x-rtp, media=(string)video, clock-rate=(int)90000, encoding-name=(string)VP8, payload=(int)97" ! rtpvp8depay ! decodebin ! videoconvert ! autovideosink
-
-const args = yargs.option("url", { default: "http://localhost:8080" }).help()
-  .argv;
+const url = process.argv[2] || "http://localhost:8080/offer";
 
 const udp = createSocket("udp4");
 udp.bind(5000);
 
-(async () => {
+new Promise<void>(async (r, f) => {
   const pc = new RTCPeerConnection({
     iceConfig: { stunServer: ["stun.l.google.com", 19302] },
   });
   const transceiver = pc.addTransceiver("video", "sendrecv");
-  transceiver.onTrack.once((track) => {
-    track.onRtp.subscribe((rtp) => {
-      udp.send(rtp.serialize(), 4002, "127.0.0.1");
-    });
-  });
 
   await pc.setLocalDescription(pc.createOffer());
-  const { data } = await axios.post(args.url + "/offer", pc.localDescription);
+  const { data } = await axios.post(url, pc.localDescription);
   pc.setRemoteDescription(data);
 
   await transceiver.sender.onReady.asPromise();
-  udp.on("message", (data) => {
-    const rtp = RtpPacket.deSerialize(data);
-    rtp.header.payloadType = pc.configuration.codecs.video![0].payloadType!;
-    transceiver.sendRtp(rtp);
+  r();
+
+  setTimeout(() => {
+    f();
+  }, 30_000);
+})
+  .then(() => {
+    console.log("done");
+    process.exit(0);
+  })
+  .catch(() => {
+    console.log("failed");
+    process.exit(1);
   });
-})();
