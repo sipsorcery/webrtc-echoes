4fb816f84c98e8c204c739db48755bf89a005e6f
shinyoshiaki <shinyoshiaki2012@gmail.com>
2021-02-25 02:03:32 +0900

    add error handling

--- werift/client.ts
+++ werift/client.ts
@@ -8,18 +8,21 @@ const udp = createSocket("udp4");
 udp.bind(5000);
 
 new Promise<any>(async (r, f) => {
+  setTimeout(() => {
+    f();
+  }, 30_000);
+
   const pc = new RTCPeerConnection({
     iceConfig: { stunServer: ["stun.l.google.com", 19302] },
   });
   pc.addTransceiver("video", "sendrecv");
 
-  await pc.setLocalDescription(pc.createOffer());
-  const { data } = await axios.post(url, pc.localDescription);
-  pc.setRemoteDescription(data);
-
-  setTimeout(() => {
-    f();
-  }, 30_000);
+  await pc.setLocalDescription(pc.createOffer()).catch((e) => f(e));
+  const { data } = await axios.post(url, pc.localDescription).catch((e) => {
+    f(e);
+    throw e;
+  });
+  pc.setRemoteDescription(data).catch((e) => f(e));
 
   pc.connectionStateChange.watch((state) => state === "connected").then(r);
 })
@@ -27,7 +30,7 @@ new Promise<any>(async (r, f) => {
     console.log("done");
     process.exit(0);
   })
-  .catch(() => {
-    console.log("failed");
+  .catch((e) => {
+    console.log("failed", e);
     process.exit(1);
   });
