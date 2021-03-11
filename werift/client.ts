e4dc6a15ac8f31860cd8ee99048f19bf0799e9cf
shinyoshiaki <shinyoshiaki2012@gmail.com>
2021-03-11 19:29:28 +0900

    werift: echo client send real RTP by gst

--- werift/client.ts
+++ werift/client.ts
@@ -1,13 +1,14 @@
-import { RTCPeerConnection } from "werift";
+import { RTCPeerConnection, RtpPacket } from "werift";
 import axios from "axios";
 import { createSocket } from "dgram";
+import { exec } from "child_process";
 
 const url = process.argv[2] || "http://localhost:8080/offer";
 
 const udp = createSocket("udp4");
 udp.bind(5000);
 
-new Promise<any>(async (r, f) => {
+new Promise<void>(async (r, f) => {
   setTimeout(() => {
     f();
   }, 30_000);
@@ -15,7 +16,13 @@ new Promise<any>(async (r, f) => {
   const pc = new RTCPeerConnection({
     iceConfig: { stunServer: ["stun.l.google.com", 19302] },
   });
-  pc.addTransceiver("video", "sendrecv");
+  const transceiver = pc.addTransceiver("video", "sendrecv");
+  transceiver.onTrack.once((track) => {
+    track.onRtp.subscribe((rtp) => {
+      console.log(rtp.header);
+      r();
+    });
+  });
 
   await pc.setLocalDescription(await pc.createOffer()).catch((e) => f(e));
   const { data } = await axios.post(url, pc.localDescription).catch((e) => {
@@ -24,7 +31,16 @@ new Promise<any>(async (r, f) => {
   });
   pc.setRemoteDescription(data).catch((e) => f(e));
 
-  pc.connectionStateChange.watch((state) => state === "connected").then(r);
+  await pc.connectionStateChange.watch((state) => state === "connected");
+  udp.on("message", (data) => {
+    const rtp = RtpPacket.deSerialize(data);
+    rtp.header.payloadType = transceiver.codecs[0].payloadType;
+    transceiver.sendRtp(rtp);
+  });
+
+  exec(
+    "gst-launch-1.0 videotestsrc ! video/x-raw,width=640,height=480,format=I420 ! vp8enc error-resilient=partitions keyframe-max-dist=10 auto-alt-ref=true cpu-used=5 deadline=1 ! rtpvp8pay ! udpsink host=127.0.0.1 port=5000"
+  );
 })
   .then(() => {
     console.log("done");
