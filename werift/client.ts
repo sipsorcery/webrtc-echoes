69895cc2c723bedf4f22b8611ba7f3ed11afd22e
shinyoshiaki <shinyoshiaki2012@gmail.com>
2021-03-18 19:58:01 +0900

    werift: client change success conditions

--- werift/client.ts
+++ werift/client.ts
@@ -32,15 +32,21 @@ new Promise<void>(async (r, f) => {
   pc.setRemoteDescription(data).catch((e) => f(e));
 
   await pc.connectionStateChange.watch((state) => state === "connected");
-  udp.on("message", (data) => {
-    const rtp = RtpPacket.deSerialize(data);
-    rtp.header.payloadType = transceiver.codecs[0].payloadType;
-    transceiver.sendRtp(rtp);
-  });
+  r();
 
-  exec(
-    "gst-launch-1.0 videotestsrc ! video/x-raw,width=640,height=480,format=I420 ! vp8enc error-resilient=partitions keyframe-max-dist=10 auto-alt-ref=true cpu-used=5 deadline=1 ! rtpvp8pay ! udpsink host=127.0.0.1 port=5000"
-  );
+  // const payloadType = transceiver.codecs.find((codec) =>
+  //   codec.mimeType.toLowerCase().includes("vp8")
+  // )!.payloadType;
+
+  // udp.on("message", (data) => {
+  //   const rtp = RtpPacket.deSerialize(data);
+  //   rtp.header.payloadType = payloadType;
+  //   transceiver.sendRtp(rtp);
+  // });
+
+  // exec(
+  //   "gst-launch-1.0 videotestsrc ! video/x-raw,width=640,height=480,format=I420 ! vp8enc error-resilient=partitions keyframe-max-dist=10 auto-alt-ref=true cpu-used=5 deadline=1 ! rtpvp8pay ! udpsink host=127.0.0.1 port=5000"
+  // );
 })
   .then(() => {
     console.log("done");
