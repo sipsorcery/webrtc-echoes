f61d10785f888087135ebf646cf8179811e13148
shinyoshiaki <shinyoshiaki2012@gmail.com>
2021-06-15 20:48:52 +0900

    werift: verup

--- werift/server.ts
+++ werift/server.ts
@@ -44,7 +44,7 @@ app.post("/offer", async (req, res) => {
   const pc = new RTCPeerConnection({
     iceServers: [{ urls: "stun:stun.l.google.com:19302" }],
   });
-  pc.onTransceiver.subscribe(async (transceiver) => {
+  pc.onRemoteTransceiverAdded.subscribe(async (transceiver) => {
     const [track] = await transceiver.onTrack.asPromise();
     pc.addTrack(track);
   });
